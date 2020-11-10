8443fcef67006d94d80bf3c6aa00e9d96a62e988
VisOk <61534601+VisOk@users.noreply.github.com>
2020-11-10 17:42:18 +0300

    add SQL scripts (create db and table)

---
+++ db/addTableUsers.sql
@@ -0,0 +1,20 @@
+-- Table: public.Users
+
+-- DROP TABLE public."Users";
+
+CREATE TABLE public."Users"
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    auth_id text COLLATE pg_catalog."default" NOT NULL UNIQUE,
+    type_id text COLLATE pg_catalog."default" NOT NULL,
+    password text COLLATE pg_catalog."default" NOT NULL,
+    token json,
+    create_date timestamp with time zone NOT NULL,
+    last_signin timestamp with time zone,
+    CONSTRAINT "Users_pkey" PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public."Users"
+    OWNER to "Whale_User";
---
+++ db/createDB.sql
@@ -0,0 +1,12 @@
+-- Database: Whale_db
+
+-- DROP DATABASE "Whale_db";
+
+CREATE DATABASE "Whale_db"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Belarusian_Belarus.1251'
+    LC_CTYPE = 'Belarusian_Belarus.1251'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
